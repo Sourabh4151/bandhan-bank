c7fe6fcdfe5551a242b2a338c26d5e6186d2db24
Sourabh4151 <sourabhtayal212@gmail.com>
2026-05-29 15:38:49 +0530

    Setup GitHub Pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/bandhan-bank/',
+
   server: {
     port: 5000,
     strictPort: true,
   },
 });
-
